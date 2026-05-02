82b7c3bdc2f28a45beddba6854725e0e1764a7f9
Yannis Nyoka <yannisnyoka@gmail.com>
2026-05-02 21:57:13 +0200

    replace

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -7,4 +8,17 @@ export default defineConfig({
       include: '**/*.{jsx,js}',
     }),
   ],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+  },
+  server: {
+    port: 5173,
+  },
+});
